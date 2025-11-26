1ee441bc4b599f23796b6d706eb7d1c9d7a184c9
tahha09 <ahmed185taha@gmail.com>
2025-11-26 15:40:37 +0200

    fixed specialization apge

---
+++ frontend/src/app/modules/public/specialties/specialties.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-specialties',
+  imports: [CommonModule],
+  standalone: true,
+  templateUrl: './specialties.html',
+  styleUrl: './specialties.scss',
+})
+export class Specialties {
+
+}
