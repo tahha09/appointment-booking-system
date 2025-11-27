90786309e5a57c9b36690f6b74d77c197856e4de
Tasneem Gaballah <tasneemgaballah16@gmail.com>
2025-11-27 19:27:05 +0200

    save edits in about us page

--- frontend/src/app/modules/public/about-us/about-us.ts
+++ frontend/src/app/modules/public/about-us/about-us.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './about-us.html',
   styleUrl: './about-us.scss',
 })
